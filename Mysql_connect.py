2bc5212ce7ff256e13c99745bee1df4bf433ac90
zhuyan <71576909+zhuzhuyan93@users.noreply.github.com>
2021-12-16 22:54:29 +0800

    Add files via upload
    连接Mysql python脚本

---
+++ Mysql_connect.py
@@ -0,0 +1,30 @@
+import MySQLdb
+import pandas as pd 
+
+class sql_operate:
+    def __init__(self, host="localhost", user="root", passwd="930428zy"):
+        self.host = host
+        self.user = user
+        self.passwd = passwd
+
+    def generate(self, sql):
+        db = MySQLdb.connect(
+            host=self.host,  # 主机名
+            user=self.user,     # 用户名
+            passwd=self.passwd,  # 密码
+            db="")    # 数据库名称
+        # 查询前，必须先获取游标
+        cur = db.cursor()
+        # 执行的都是原生SQL语句
+        cur.execute(sql)
+        result = cur.fetchall()
+        columns = [i[0] for i in cur.description]
+        df = pd.DataFrame(result, columns=columns)
+        db.close() 
+        return df, result, columns 
+
+sql1 = 'select * from world.city limit 100'
+
+connect_sql = sql_operate()
+
+connect_sql.generate(sql1)[0] 
